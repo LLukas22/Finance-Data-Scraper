8337d23ce441736de1567a9c5dd9afcb8081a583
LLukas22 <65088241+LLukas22@users.noreply.github.com>
2022-08-14 16:45:43 +0200

    Fixed Setup

--- src/news/setup.py
+++ src/news/setup.py
@@ -2,8 +2,7 @@ from setuptools import setup,find_packages
 import os
 
 setup(
-    version=os.environ.get("PACKAGE_VERSION","DEBUG"),
-    packages=find_packages(exclude=("tests",)),
-    include_package_data=True, 
-    exclude_package_data={'': ['tests']}
+    version=os.environ.get("PACKAGE_VERSION","0.0.0"),
+    package_dir={"":"src"},
+    packages=find_packages(where="./src", exclude=("*.tests", "*.tests.*", "tests.*", "tests"))
     )
